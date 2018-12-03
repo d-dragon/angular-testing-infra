64c94158fab45cc939235f64d50c002018a0e208
Lee Khai <leekhai18@gmail.com>
2018-12-03 15:45:30 +0700

    Config for unit test report. Using junit and ChromeHeadless

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'BEC-V2-APP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('BEC-V2-APP');
+    expect(app.title).toEqual('1BEC-V2-APP');
   });
 
   it('should render title in a h1 tag', () => {
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,7 +9,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,12 +20,26 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
+    angularCli: {
+      enviroment: 'dev',
+    },
     reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'reports', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'TESTS-ChromeHeadless-UnitTest.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      useBrowserName: false // add browser name to report and classes names
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
