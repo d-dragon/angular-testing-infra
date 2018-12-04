4fa4ef5fbaf0fb970e92d782b9bc70e614d267be
Duy Phan <phanduyt291@gmail.com>
2018-12-04 14:46:41 +0700

    Reconfigure testing browser by using ChromeHeadlessNoSandbox supported azure build agent

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -33,9 +33,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
